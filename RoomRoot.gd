4b2e641d27cf6de4d9fddb25bc176352a7fe2ba2
dralimazing <67755891+dralimazing@users.noreply.github.com>
2023-01-22 20:16:05 +0300

    sound death fade

--- RoomRoot.gd
+++ RoomRoot.gd
@@ -2,11 +2,13 @@ extends Spatial
 
 
 var itemMap = {'wheel': 2, 'rocket':2, 'balloon': 3, 'piston': 1};
-
+var volume = 0;
 onready var audio = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	audio.play()
+	audio.play();
+	volume = 0;
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('Master'), volume);
 	if GlobalVar.currentLevel=='level1':
 		itemMap = {'wheel': 10, 'rocket':10, 'balloon': 10, 'piston': 10};
 	elif GlobalVar.currentLevel == 'level2':
@@ -21,7 +23,9 @@ func _ready():
 		itemMap = {'wheel': 2, 'rocket':1, 'balloon': 2, 'piston': 2};
 func _physics_process(delta):
 	if $deathBed.overlaps_body($Spatial) || $Spatial.global_translation.y< -50:
-		reloadScene()
+		fadeAudio();
+		if volume <= -80:
+			reloadScene();
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -37,3 +41,7 @@ func reloadScene():
 	itemMap = {'wheel': 2, 'rocket':2, 'balloon': 3, 'piston': 1};
 	GlobalVar.playing = false;
 	get_tree().reload_current_scene();
+
+func fadeAudio():
+	volume -=2;
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('Master'), volume);
